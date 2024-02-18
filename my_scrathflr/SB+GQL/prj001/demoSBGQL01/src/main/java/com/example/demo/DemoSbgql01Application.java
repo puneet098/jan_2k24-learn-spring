43442c04fe65db93c1d9dbb68feb9e92a7790787
puneet098 <puneet098@hotmail.com>
2024-02-18 18:42:28 +0530

    18 Feb checking 2

---
+++ my_scrathflr/SB+GQL/prj001/demoSBGQL01/src/main/java/com/example/demo/DemoSbgql01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbgql01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSbgql01Application.class, args);
+	}
+
+}
---
+++ my_scrathflr/myfirstwebapp_bk/myfirstwebapp/target/classes/META-INF/resources/WEB-INF/jsp/sayWelcome.jsp
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title> Welcome Page - JSP</title>
+	</head>
+	<body>
+		My Welcome page with body - JSP
+	</body>
+</html>
